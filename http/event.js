5b7f3c613a629e0462bf62a75f2280d99db89d2f
HelloWorld <nidakun@163.com>
2017-07-13 21:48:27 +0800

    EventEmitter

--- http/event.js
+++ http/event.js
@@ -1,7 +1,78 @@
-function clickIt(e){
-	Window.alert('Button is clicked.')
+// function clickIt(e){
+// 	Window.alert('Button is clicked.')
+// }
+
+// var button = document.getElementById('#button');
+
+// button.addEventListener(clickIt);
+// 
+
+var EventEmitter=require('events').EventEmitter
+var life=new EventEmitter()
+
+life.setMaxListeners(11)
+
+function water(who){
+	console.log('给 ' +who+ ' ..1')
 }
 
-var button = document.getElementById('#button');
+life.on('submit',water)
 
-button.addEventListener(clickIt);
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..2')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..3')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..4')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..5')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..6')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..7')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..8')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..9')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..10')
+})
+
+life.on('submit',function(who){
+	console.log('给 ' +who+ ' ..11')
+})
+
+life.on('cancle',function(who){
+	console.log('给 ' +who+ ' ..a')
+})
+
+life.on('cancle',function(who){
+	console.log('给 ' +who+ ' ..b')
+})
+
+life.removeListener('submit',water);
+life.removeAllListeners('submit')
+console.log(EventEmitter.listenerCount(life,'submit'))
+
+var hasSubmitListener= life.emit('submit','汉子')
+
+var hasCancleListener=life.emit('cancle','妹子')
+
+console.log(hasSubmitListener)
+console.log(hasCancleListener)
